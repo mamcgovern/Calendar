9a1d6d398ae9ea977aec61683131b49319a0a816
Mattie <mrm4@iastate.edu>
2024-07-13 18:05:16 -0500

    working on login

--- calendar/src/backend/server.js
+++ calendar/src/backend/server.js
@@ -16,8 +16,8 @@ const host = "localhost";
 const { MongoClient } = require("mongodb");
 
 // MongoDB
-const url = "mongodb://127.0.0.1:27017";
-const dbName = "TSTourTracker";
+const url = "mongodb+srv://Cluster42981:L1ttlelady@hyperfixationscalendar.bfpjkxl.mongodb.net/?retryWrites=true&w=majority&appName=HyperfixationsCalendar";
+const dbName = "HyperfixationsCalendar";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
@@ -28,6 +28,47 @@ app.listen(port, () => {
 // ███████████████████████████████████████████████████████████████████████
 // SECTION: Accounts
 // ███████████████████████████████████████████████████████████████████████
+
+// Get all accounts
+app.get("/accounts", async (req, res) => {
+    await client.connect();
+    console.log("Node connected successfully to GET MongoDB");
+    const query = {};
+    const results = await db
+        .collection("Accounts")
+        .find(query)
+        .toArray();
+    console.log(results);
+    res.status(200);
+    res.send(results);
+});
+
+// Get account by username
+app.get("/accounts/:username", async (req, res) => {
+    const username = req.params.username;
+    
+    try {
+        await client.connect();
+        console.log("Node connected successfully to GET MongoDB");
+        
+        const query = { username: username };
+        const result = await db
+            .collection("Accounts")
+            .findOne(query);
+        
+        if (result) {
+            console.log(result);
+            res.status(200).send(result);
+        } else {
+            res.status(404).send({ message: "Account not found" });
+        }
+    } catch (error) {
+        console.error("Error connecting to MongoDB or fetching data", error);
+        res.status(500).send({ message: "Internal server error" });
+    } finally {
+        await client.close();
+    }
+});
 
 
 // ███████████████████████████████████████████████████████████████████████
@@ -54,13 +95,13 @@ app.listen(port, () => {
 // SECTION: Events
 // ███████████████████████████████████████████████████████████████████████
 
-// Get all events
-app.get("/events", async (req, res) => {
+// Get all accounts
+app.get("/accounts", async (req, res) => {
     await client.connect();
     console.log("Node connected successfully to GET MongoDB");
     const query = {};
     const results = await db
-        .collection("events")
+        .collection("Accounts")
         .find(query)
         .toArray();
     console.log(results);
--- calendar/src/components/Login.js
+++ calendar/src/components/Login.js
@@ -5,11 +5,105 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Login = () => {
-    return (
-        <div className='container'>
+    const [loggedin, setLoggedin] = useState(false);
+    const [incorrect, setincorrect] = useState(false);
+    const [username, setUsername] = useState("user");
+    const [password, setPassword] = useState("pass");
+    const [userInfo, setUserInfo] = useState([]);
 
-        </div>
-    );
+    const form = useRef('form');
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        const formData = new FormData(form.current);
+        setUsername(formData.get('username'));
+        setPassword(formData.get('password'));
+
+            var url = "http://localhost:8081/accounts/" + username; 
+            console.log(url);
+            console.log(formData.get('username'));
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("User info: ", data);
+                    setUserInfo(data);
+                });
+        if(userInfo.password === password) {
+            setLoggedin(true);
+        } else{
+            setincorrect(true);
+            console.log("Real password: " + userInfo.password);
+        }
+    };
+
+    if(loggedin) {
+        return (
+            <div className='container'>
+                Hello
+            </div>
+        );
+    } else {
+        if(incorrect){
+            return (
+                <div class='container'>
+                    <div class="bg-body-tertiary p-5 rounded">
+                        <div class="col-sm-8 mx-auto">
+                            <form ref={form} onSubmit={submitForm}>
+                                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+                                <p>Incorrect username or password. Try again.</p>
+        
+                                <div class="form-floating">
+                                    <input type="username" class="form-control" id="floatingInput" placeholder="Username" name="username" />
+                                    <label for="floatingInput">Username</label>
+                                </div>
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" />
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+        
+                                <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                                <a style={{ marginTop: '20px' }} href="/account/forgotpassword">Forgot password?</a>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="bg-body-tertiary p-5 rounded" style={{ marginTop: '20px' }}>
+                        <div class="col-sm-8 mx-auto">
+                            <p>Don't have an account? <a style={{ marginTop: '20px' }} href="/account/signup">Sign up</a></p>
+                        </div>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div class='container'>
+                    <div class="bg-body-tertiary p-5 rounded">
+                        <div class="col-sm-8 mx-auto">
+                            <form ref={form} onSubmit={submitForm}>
+                                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        
+                                <div class="form-floating">
+                                    <input type="username" class="form-control" id="floatingInput" placeholder="Username" name="username"/>
+                                    <label for="floatingInput">Username</label>
+                                </div>
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" />
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+        
+                                <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                                <a style={{ marginTop: '20px' }} href="/account/forgotpassword">Forgot password?</a>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="bg-body-tertiary p-5 rounded" style={{ marginTop: '20px' }}>
+                        <div class="col-sm-8 mx-auto">
+                            <p>Don't have an account? <a style={{ marginTop: '20px' }} href="/account/signup">Sign up</a></p>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
 
 export default Login;
